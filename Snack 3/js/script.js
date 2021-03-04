e4c1ff799c4a62007466dcdf509fe1d8748e24f0
Tommaso Borsatti <tommasoborsatti89@gmail.com>
2021-03-04 13:03:11 +0100

    Snack 3 - Blocco 2 - Correzione
    Aggiunta la determinazione di un nuovo Array contenente i risultati generati in modo randomico.

--- Snack 3/js/script.js
+++ Snack 3/js/script.js
@@ -8,15 +8,22 @@ falsa lista di 3 invitati.
 
 var cognomi = ["Inzaghi", "Morbidozzi", "Lardi", "Peloncini", "Mammalucchi"];
 var nomi = ["Tommaso", "Bobby", "Mimmo", "Tony", "Rigoberto"];
+var invitati = []
 var nomeCasuale = 0;
 var j=0;
 var r=0;
 
 for (var i = 0; i < 3; i++){
 
-  j = Math.floor(Math.random() * 5);
-  r = Math.floor(Math.random() * 5);
+  j = Math.floor(Math.random() * nomi.length);
+  r = Math.floor(Math.random() * cognomi.length);
   nomeCasuale = nomi[j] + " " + cognomi[r];
+
+  invitati.push(nomeCasuale);
+
   console.log(nomeCasuale);
   document.getElementById('lista').innerHTML += "<li>" + nomeCasuale + "</li>"
 }
+
+console.log(invitati);
+  
