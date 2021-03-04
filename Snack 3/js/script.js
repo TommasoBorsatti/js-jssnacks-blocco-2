5d6aa3753dbf60a3e475d67fe08f56565f129b9b
Tommaso Borsatti <tommasoborsatti89@gmail.com>
2021-03-04 12:36:34 +0100

    Snackr 3 - Blocco 2 - Script Fatto

---
+++ Snack 3/js/script.js
@@ -0,0 +1,22 @@
+
+/*
+Generatore di “nomi cognomi” casuali: prendendo una lista
+di nomi e una lista di cognomi, Gatsby vuole generare una
+falsa lista di 3 invitati.
+*/
+
+
+var cognomi = ["Inzaghi", "Morbidozzi", "Lardi", "Peloncini", "Mammalucchi"];
+var nomi = ["Tommaso", "Bobby", "Mimmo", "Tony", "Rigoberto"];
+var nomeCasuale = 0;
+var j=0;
+var r=0;
+
+for (var i = 0; i < 3; i++){
+
+j = parseInt(Math.random() * 5)
+r = parseInt(Math.random() * 5);
+  nomeCasuale = nomi[j] + " " + cognomi[r];
+  console.log(nomeCasuale);
+  document.getElementById('lista').innerHTML += "<li>" + nomeCasuale + "</li>"
+}
